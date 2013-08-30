d083e96b1f5abdac85f57ca535e5f0f547f1f1e8
Ryan Murphy <github@accounts.rdmurphy.org>
2013-08-30 11:51:50 -0500

    Added grunt watch support

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,4 +28,5 @@ module.exports = function(grunt) {
 
     grunt.registerTask('test', ['jshint']);
     grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('observe', ['watch']);
 };
